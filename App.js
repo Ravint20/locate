f01f1815c691663d3281b8a1ba481911e6d91bbe
Ravint20 <Ravinsant20@gmail.com>
2018-07-25 17:15:03 +0530

    updated

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View,TextInput } from 'react-native';
 import Input from './components/Input';
-import Map from "./components/Map";
+import {MapView} from "expo";
+
 
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-       <View><Input /></View>
-       <View><Map /></View>
+        <Input />
+      <MapView 
+       style={styles.map}
+       initialRegion={{
+        latitude: 6.9173,
+        longitude: 79.8484,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+       />
       </View>
       
     )
@@ -19,9 +28,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    backgroundColor: '#fff',
+   
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  map:{
+
+    width:"100%",
+    height:"100%"
+  }
 
 });
