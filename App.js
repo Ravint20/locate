18ecac90587b134743a3204514a5f41a84dd918f
Ravint20 <Ravinsant20@gmail.com>
2018-07-25 15:29:54 +0530

    updated

--- App.js
+++ App.js
@@ -1,37 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View,TextInput } from 'react-native';
-import {MapView} from "expo";
+import Input from './components/Input';
+import Map from "./components/Map";
 
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
-     <View style={styles.input}>
-      <TextInput
-
-       placeholder="search on google map" 
-       
-      />
+       <View><Input /></View>
+       <View><Map /></View>
       </View>
-      <View style={{top:10}}>
-       <MapView 
-      
-        style={{ alignSelf: 'stretch', height: 500, width:400 }}
-
-    
-      
-      initialRegion={{
-        latitude: 6.9173,
-        longitude: 79.8484,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-       />
-       </View>
-
-       </View>
       
     )
   }
@@ -44,12 +23,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input:{
 
-    width:330,
-    height:50,
-    padding:10,
-    backgroundColor:"white",
-    opacity:1
-  }
 });
