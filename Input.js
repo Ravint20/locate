7d5fd8a42f67199f826f401c3ee9c5b68f0ce870
Ravint20 <Ravinsant20@gmail.com>
2018-07-25 17:14:33 +0530

    Update Input.js

--- Input.js
+++ Input.js
@@ -3,22 +3,27 @@ import {Text,StyleSheet,View,TextInput} from 'react-native';
 
 export default class Input extends Component {
     render(){
+     
      return(
 
 <View style={styles.input}>
-      <TextInput
-        placeholder="search on google map" 
-       />
+  <TextInput
+    placeholder="search on google map" 
+   />
 </View>
          )
 }}
 
 const styles=StyleSheet.create({
 input:{
-    width:330,
+    width:300,
     height:50,
     padding:10,
     backgroundColor:"white",
+    marginTop:200,
+    marginLeft:10,
+    marginRight:10,
+    borderStyle:"solid",
     opacity:0.7
   },
   
