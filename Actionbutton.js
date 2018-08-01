52a19a5441a62de826c49d1fd0f1af4b016c54a5
Ravint20 <Ravinsant20@gmail.com>
2018-08-01 11:42:22 +0530

    Add files via upload

---
+++ Actionbutton.js
@@ -0,0 +1,23 @@
+import React,{Component} from "react";
+import {Text,StyleSheet,View,TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import ActionButton from 'react-native-action-button';
+
+export default class Actionbutton extends Component {
+
+render(){
+
+return(
+
+<ActionButton 
+   size={80}
+  buttonColor="rgba(231,76,60,1)"
+  onPress={() => { console.log("hi")}}
+/>
+
+)
+
+}
+
+}
+
