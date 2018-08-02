a9884f67c4dbc5fb3559eea197d9970e86166ae3
Ravint20 <Ravinsant20@gmail.com>
2018-08-02 23:50:20 +0530

    updated on 2/08 11.49pm

--- App.js
+++ App.js
@@ -1,50 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View,TextInput} from 'react-native';
+import { StyleSheet, Text, View,TextInput,Platform} from 'react-native';
 import Input from './components/Input';
-import {MapView} from "expo";
+import Map from './components/Map';
 import Actionbutton from './components/Actionbutton';
 import PickerSelect from './components/Picker';
-
+import Container from './components/Container';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-     
-   
-
-
-
-        
-      <MapView 
-       style={styles.map}
-       initialRegion={{
-        latitude: 6.9173,
-        longitude: 79.8484,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-       
-       />
-
-        <View>
-    <PickerSelect />
-</View> 
-
-  <View style={styles.input}>
-    <Input />
-
-</View>
 
        
+         <Map />
+         <PickerSelect />
+        <Input />
+         <Container />
+        <Actionbutton />
+
       
-
-      <View style={styles.box}>
-
-      </View>
-
-       <Actionbutton />
-
       </View>
       
     )
@@ -55,29 +29,6 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
    
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  input:{
     
-     
-
   },
-
-  map:{
-   width:"100%",
-    height:"100%"
-  },
-
-  box:
-  {
-    justifyContent:"flex-end",
-    width:"100%",
-    height:"50%",
-    backgroundColor:"#ffffff",
-    opacity:0.5
-
-  }
-
 });
