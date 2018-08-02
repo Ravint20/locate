fbe5dcc1a1ce7f0673e2d0b89e3c4775baafc189
Ravint20 <Ravinsant20@gmail.com>
2018-08-03 01:37:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -10,11 +10,11 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
+           <PickerSelect />
+           <Input />
        
-         <Map />
-         <PickerSelect />
-        <Input />
+             <Map />
+        
          <Container />
         <Actionbutton />
 
@@ -28,6 +28,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex:1,
+   justifyContent:"center",
    
     
   },
