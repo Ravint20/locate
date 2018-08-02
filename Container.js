5a637b191a9ee40dd485d761773ecbd07a094d69
Ravint20 <Ravinsant20@gmail.com>
2018-08-02 23:48:31 +0530

    Add files via upload

---
+++ Container.js
@@ -0,0 +1,35 @@
+import React,{Component} from "react";
+import {Text,StyleSheet,View,TextInput,Picker,Modal} from 'react-native';
+
+export default class Container extends Component {
+
+    render()
+
+    {
+
+     return(
+
+        <View style={styles.box}>
+
+        </View>
+
+
+     )
+
+    }
+    }
+
+    const styles = StyleSheet.create({
+
+        box:
+        { flex:1,
+          justifyContent:"flex-end",
+          width:"100%",
+          height:"50%",
+          backgroundColor:"#ffffff",
+          opacity:0.5
+      
+        }
+
+
+    })
