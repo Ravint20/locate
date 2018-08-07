312716281e69b59cff7532920adc6b2423e0bb8c
Ravint20 <Ravinsant20@gmail.com>
2018-08-07 10:13:14 +0530

    Add files via upload

---
+++ GooglePlacesInput.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react';
+import { Constants } from 'expo';
+import { View } from 'react-native';
+import {
+  GooglePlacesAutocomplete,
+} from 'react-native-google-places-autocomplete'; // 1.2.12
+
+const homePlace = {
+  description: 'Home',
+  geometry: { location: { lat: 48.8152937, lng: 2.4597668 } },
+};
+
+const workPlace = {
+  description: 'Work',
+  geometry: { location: { lat: 48.8496818, lng: 2.2940881 } },
+};
+
+
+export default class GooglePlacesInput extends Component{
+  render() {
+
+    return (
+      <View style={{  flex: 0.125 }}>
+
+        <GooglePlacesAutocomplete
+          placeholder="Search on google map"
+          minLength={2} 
+          autoFocus={false}
+          returnKeyType={'search'} 
+          listViewDisplayed="auto" 
+          fetchDetails={true}
+          renderDescription={row => row.description} // custom description render
+          onPress={(data, details = null) => {
+            
+            console.log(data);
+            console.log(details);
+          }}
+
+          getDefaultValue={() => {
+            return ''; // text input default value
+          }}
+          
+          query={{
+            
+            key: 'AIzaSyC2QhtACfVZ2cr9HVvxQuzxd3HT36NNK3Q',
+            language: 'en', 
+            types: '(cities)', 
+          }}
+          styles={{
+            description: {
+              fontWeight: 'bold',
+            },
+            predefinedPlacesDescription: {
+              color: '#1faadb',
+            },
+          }}
+          currentLocation={true} 
+          currentLocationLabel="Current location"
+          nearbyPlacesAPI="GooglePlacesSearch" 
+          GoogleReverseGeocodingQuery={{
+           
+          }}
+          GooglePlacesSearchQuery={{
+           
+            rankby: 'distance',
+            types: 'food',
+          }}
+          filterReverseGeocodingByTypes={[
+            'locality',
+            'administrative_area_level_3',
+          ]} 
+          predefinedPlaces={[homePlace, workPlace]}
+          debounce={200}
+        />
+      </View>
+    );
+  }
+}
