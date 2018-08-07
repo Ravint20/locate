9746b613fa70b860257748b5054e56546cee4567
Ravint20 <Ravinsant20@gmail.com>
2018-08-07 10:12:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View,TextInput,Platform} from 'react-native';
-import Input from './components/Input';
+import GooglePlacesInput from './components/GooglePlacesInput';
 import Map from './components/Map';
 import Actionbutton from './components/Actionbutton';
 import PickerSelect from './components/Picker';
@@ -10,10 +10,11 @@ import Thermometer from './components/Thermometer';
 export default class App extends React.Component {
   render() {
     return (
+      
       <View style={styles.container}>
-           <PickerSelect />
-           <Input />
-           <Map />
+          <PickerSelect />
+          <GooglePlacesInput />
+          <Map />
          <Container />
         <Actionbutton />
         <Thermometer />
