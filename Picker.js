52e1a6a90fdee4c2afb7ed0f8f170c273734e43a
Ravint20 <Ravinsant20@gmail.com>
2018-08-24 14:51:26 +0530

    Update Picker.js

--- Picker.js
+++ Picker.js
@@ -42,32 +42,33 @@ export default class PickerSelect extends Component {
         super(props);
         this.state = {
           
-          language: 'hi',
+          language: '',
         };
       }
-    render(){
+    render(){      
      
  return(
 
-    <View style={styles.container}>
-    <Picker  style={{padding:10,width:"100%",backgroundColor:"#434343"}} mode="dropdown"
+    <View style={styles.container}>         
+    <Picker  style={{padding:20,width:"100%",backgroundColor:"#434343"}} itemStyle={{paddingHorizontal:20,textAlign:"center"}} mode="dropdown"  
       
       selectedValue={this.state.language}
       onValueChange={itemValue => this.setState({ language: itemValue })}>
-
-        <Picker.Item color="#FF9900" label="SELECT CONTAINER" value="SELECT CONTAINER" /> 
+        
+        <Picker.Item   color="#FF9900" label="SELECT CONTAINER" value="SELECT CONTAINER" /> 
+        
        <Picker.Item color="#FF9900" label="Java" value="java" /> 
-       <Picker.Item  color="#FF9900" label="JavaScript" value="js" />   
-    
+       <Picker.Item  color="#FF9900" label="JavaScript" value="js" />        
+         
  </Picker> 
-
+          
  <Triangle /> 
 
 {/*
 <Modal visible={true} transparent={true} style={{backgroundColor:"efefef"}} onRequestClose={() => console.log("hi")}></Modal>
 */}
-  </View>
-
+  </View>       
+ 
  )}}
 
  
