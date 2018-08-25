afe28729e69dc51f6b6b62b297d01a2237247b30
Ravint20 <Ravinsant20@gmail.com>
2018-08-25 11:50:44 +0530

    Update Picker.js

--- Picker.js
+++ Picker.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-import {Text,StyleSheet,View,TextInput,Picker,Modal} from 'react-native';
+import {Text,StyleSheet,View,TextInput,Picker,Modal,ScrollView} from 'react-native';
 import Triangle from './Triangle';
 const programmingLanguages = [
     {
@@ -43,31 +43,63 @@ export default class PickerSelect extends Component {
         this.state = {
           
           language: '',
+          labelName:" ",
+        
         };
       }
-    render(){      
-     
+
+      onValueChange2(value,label){
+      
+
+        this.setState(
+          {
+         language:value,
+         labelName:label
+
+          }
+        )
+
+
+      }
+    render(){    
+      
+      const description =[]
+
+      {
+
+        programmingLanguages.map((y,x) =>{
+
+          description.push(<Picker.Item label={y.label} key={x} value={y.label} />)
+
+        })
+
+      }
+        
  return(
 
-    <View style={styles.container}>         
+    <ScrollView style={styles.container}> 
+
+    
+        
     <Picker  style={{padding:20,width:"100%",backgroundColor:"#434343"}} itemStyle={{paddingHorizontal:20,textAlign:"center"}} mode="dropdown"  
       
       selectedValue={this.state.language}
-      onValueChange={itemValue => this.setState({ language: itemValue })}>
+      onValueChange={() => this.onValueChange2}>
         
-        <Picker.Item   color="#FF9900" label="SELECT CONTAINER" value="SELECT CONTAINER" /> 
-        
-       <Picker.Item color="#FF9900" label="Java" value="java" /> 
-       <Picker.Item  color="#FF9900" label="JavaScript" value="js" />        
-         
- </Picker> 
+       {description} 
+
+       </Picker> 
+       
           
- <Triangle /> 
+         
+ 
+             
+ <Triangle />    
 
 {/*
 <Modal visible={true} transparent={true} style={{backgroundColor:"efefef"}} onRequestClose={() => console.log("hi")}></Modal>
 */}
-  </View>       
+  </ScrollView>       
  
  )}}
 
